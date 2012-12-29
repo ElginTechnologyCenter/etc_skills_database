1df19b69d101580f0f59fda61d71184c4807b2c2
Justin Love <git@JustinLove.name>
2012-12-29 12:54:07 -0600

    dont check in secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EtcSkillDatabase::Application.config.secret_token = '7310c7ee9063e74d53cc416bcb04f2fe05610512d917cfb3b9b92ea35cf886c6c12821fe2442e1cadef118d35660a76b7b8ec93f43bc51af47a5646227441962'
+EtcSkillDatabase::Application.config.secret_token = ENV['SECRET_TOKEN']
