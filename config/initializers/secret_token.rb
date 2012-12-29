c892d8fcc70adf3e6fa786bcf08ebc2e9ab5afb5
Justin Love <git@JustinLove.name>
2012-12-29 12:30:10 -0600

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EtcSkillDatabase::Application.config.secret_token = '7310c7ee9063e74d53cc416bcb04f2fe05610512d917cfb3b9b92ea35cf886c6c12821fe2442e1cadef118d35660a76b7b8ec93f43bc51af47a5646227441962'
